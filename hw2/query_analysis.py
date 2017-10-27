ebbb666e6f206b8533d2754b1290693c4905b38e
Mergen <imeevma@gmail.com>
2017-10-27 03:36:23 +0300

    +Tree by stream

--- hw2/query_analysis.py
+++ hw2/query_analysis.py
@@ -21,6 +21,18 @@ class QtreeTypeInfo:
 class QTreeTerm(QtreeTypeInfo):
     def __init__(self, term):
         QtreeTypeInfo.__init__(self, term, term=True)
+        self.current_doc_id = 0
+
+    def get_values(self, search):
+        self.value = sorted(search.search_word(self.value))
+
+    def goto(self, doc_id):
+        self.value = [x for x in self.value if x >= doc_id]
+
+    def evaluate(self):
+        if self.value:
+            return self.value[0]
+        return self.value
 
 
 class QTreeOperator(QtreeTypeInfo):
@@ -29,6 +41,46 @@ class QTreeOperator(QtreeTypeInfo):
         self.priority = get_operator_prio(op)
         self.left = None
         self.right = None
+        self.current_doc_id = -1
+
+    def goto(self, doc_id):
+        if self.left:
+            self.left.goto(doc_id)
+        self.right.goto(doc_id)
+        self.current_doc_id = doc_id
+
+    def evaluate(self):
+        if self.value == "|":
+            left = self.left.evaluate()
+            right = self.right.evaluate()
+            if not left:
+                return right
+            if not right:
+                return left
+            return min(left, right)
+
+        if self.value == "&":
+            left = self.left.evaluate()
+            right = self.right.evaluate()
+            while left and right and left != right:
+                doc_next = max(left, right)
+                self.left.goto(doc_next)
+                self.right.goto(doc_next)
+                left = self.left.evaluate()
+                right = self.right.evaluate()
+            if left and left == right:
+                return left
+            return list()
+
+        if self.value == '!':
+            val = self.right.evaluate()
+            doc_id = self.current_doc_id
+            while (doc_id == val):
+                doc_id += 1
+                self.right.goto(doc_id)
+                val = self.right.evaluate()
+            return doc_id
+
 
 
 class QTreeBracket(QtreeTypeInfo):
@@ -113,47 +165,50 @@ def parse_query(q):
     tokens = tokenize_query(q)
     return build_query_tree(tokens)
 
-def qtree2str(root, depth=0):
-    if root.is_operator:
-        need_brackets = depth > 0 and root.value != '!'
-        res = ''
-        if need_brackets:
-            res += '('
-        if root.left:
-            res += qtree2str(root.left, depth+1)
-        if root.value == '!':
-            res += root.value
-        else:
-            res += ' ' + root.value + ' '
-        if root.right:
-            res += qtree2str(root.right, depth+1)
-        if need_brackets:
-            res += ')'
-        return res
-    else:
-        return root.value
+
+
+
 
 class Query:
     def __init__(self, search):
         self.search = search
 
     def handle_query(self, query):
-        return self.execute_query(parse_query(query.lower()))
+        root = parse_query(query.lower())
+        self.set_values(root)
+        res = list()
+        item = root.evaluate()
+        while(item):
+            res.append(item)
+            root.goto(item + 1)
+            item = root.evaluate()
+        return res
 
-    def execute_query(self, root, depth=0):
+    def set_values(self, root):
         if root.is_operator:
-            if root.value == '&':
-                left = self.execute_query(root.left, depth + 1)
-                right = self.execute_query(root.right, depth + 1)
-                if root.left.value == '!':
-                    return right - left
-                if root.right.value == '!':
-                    return left - right
-                return left & right
-            if root.value == '|':
-                left = self.execute_query(root.left, depth + 1)
-                right = self.execute_query(root.right, depth + 1)
-                return left | right
-            return self.execute_query(root.right, depth + 1)
-        else:
-            return self.search.search_word(root.value)
+            if root.left:
+                self.set_values(root.left)
+            self.set_values(root.right)
+            return
+        root.get_values(self.search)
+
+    # def handle_query(self, query):
+    #     return self.execute_query(parse_query(query.lower()))
+
+    # def execute_query(self, root, depth=0):
+    #     if root.is_operator:
+    #         if root.value == '&':
+    #             left = self.execute_query(root.left, depth + 1)
+    #             right = self.execute_query(root.right, depth + 1)
+    #             if root.left.value == '!':
+    #                 return right - left
+    #             if root.right.value == '!':
+    #                 return left - right
+    #             return left & right
+    #         if root.value == '|':
+    #             left = self.execute_query(root.left, depth + 1)
+    #             right = self.execute_query(root.right, depth + 1)
+    #             return left | right
+    #         return self.execute_query(root.right, depth + 1)
+    #     else:
+    #         return self.search.search_word(root.value)
